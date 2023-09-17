f6fb42e13a6a193cf7ff644172b7a7a347e7df9e
Seth Gregory <sethgregory@seths-mbp-2.mynetworksettings.com>
2023-09-16 22:28:57 -0400

    Added comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :posts
+  resources :posts do
+    resources :comments  
+  end
+  
   root "posts#index"
 
 end
